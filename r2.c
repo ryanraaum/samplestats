445167537ce892a5ce2f4921187a5b56e0c2ee97
Ryan Raaum <github@raaum.org>
2009-02-23 13:25:11 -0500

    normalize formatting

--- r2.c
+++ r2.c
@@ -12,7 +12,8 @@
  *
  *  Returns nothing (fills in the array given)
  */
-void count_binary_unic_frequencies( int nsam, int segsites, char **list, int *site_freqs, int *unic_freqs ) {
+void count_binary_unic_frequencies(int nsam, int segsites, char **list, int *site_freqs, int *unic_freqs) 
+{
     int     i, j;               /* iterators */
 
     /* first initialize all unic counts to 0 */
@@ -22,13 +23,13 @@ void count_binary_unic_frequencies( int nsam, int segsites, char **list, int *si
 
     /* If there are no segregating sites, then there are no unique sites. 
      * So only do the work if there are some segregating sites. */
-    if ( segsites > 0 ) {
+    if (segsites > 0) {
         /* step through the site frequency spectrum, checking all unique sites */
-        for (i=0; i < segsites; i++) {
+        for (i=0; i<segsites; i++) {
             /* every time we find a site with frequency 1, find the sequence that is
              * the source of that site and up its' unic_freqs count. */
             if (site_freqs[i] == 1) {
-                for(j=0; j < nsam; j++) {
+                for (j=0; j<nsam; j++) {
                     if (list[j][i] == '1') {
                         unic_freqs[j] += 1;
                     }
@@ -50,27 +51,28 @@ void count_binary_unic_frequencies( int nsam, int segsites, char **list, int *si
  *
  *  Returns nothing (fills in the array given)
  */
-void count_agct_unic_frequencies( int nsam, int segsites, char **list, int site_freqs[][4], int *unic_freqs ) {
+void count_agct_unic_frequencies(int nsam, int segsites, char **list, int site_freqs[][4], int *unic_freqs) 
+{
     int     i, j, k;               /* iterators */
     char    *agct;
 
     agct = "AGCT\0";
 
     /* first initialize all unic counts to 0 */
-    for (i=0; i < nsam; i++) {
+    for (i=0; i<nsam; i++) {
         unic_freqs[i] = 0;
     }
 
     /* If there are no segregating sites, then there are no unique sites. 
      * So only do the work if there are some segregating sites. */
-    if ( segsites > 0 ) {
+    if (segsites > 0) {
         /* step through the site frequency spectrum, checking all unique sites */
-        for (i=0; i < segsites; i++) {
+        for (i=0; i<segsites; i++) {
             for (j=0; j<4; j++) {
                 /* every time we find a site with frequency 1, find the sequence that is
                  * the source of that site and up its' unic_freqs count. */
                 if (site_freqs[i][j] == 1) {
-                    for(k=0; k < nsam; k++) {
+                    for (k=0; k<nsam; k++) {
                         if (list[k][i] == agct[j]) {
                             unic_freqs[k] += 1;
                         }
@@ -84,17 +86,19 @@ void count_agct_unic_frequencies( int nsam, int segsites, char **list, int site_
 /*R2 Ramos & Rozas: "*unic" is the number of singletons in each sequence (in comparison to the sample studied)*/	
 double R2(int *unic, double pi, int sample_size, int S)
 {
-    double sm2 = 0.0;
-    int i;
+    double  sm2;
+    int     i;
+
+    sm2 = 0.0;
     
     if(S == 0 || sample_size == 0) return(-10000);
-    for (i=0;i<sample_size;i++)
-            sm2 += ((double)unic[i] - pi/2.0)*((double)unic[i] - pi/2.0);
+    for (i=0; i<sample_size; i++)
+        sm2 += ((double)unic[i] - pi/2.0)*((double)unic[i] - pi/2.0);
     
     sm2 = sqrt(sm2/((double)sample_size))/(double)S;
             
     if (sm2 < 1.0E-15)
-            sm2 = 0.0;
+        sm2 = 0.0;
 
     return (double)sm2;
 }
